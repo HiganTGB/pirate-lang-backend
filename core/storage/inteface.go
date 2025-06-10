d1700aa231ceb2a0223620e9d9270649eda4b5f7
HiganTGB <100978108+HiganTGB@users.noreply.github.com>
2025-06-10 21:41:45 +0700

    Update inteface.go

--- core/storage/inteface.go
+++ core/storage/inteface.go
@@ -12,16 +12,4 @@ type IStorage interface {
 	// Avatar Operations
 	BuildAvatarURL(objectName string) string
 	UploadAvatar(ctx context.Context, userID uuid.UUID, file io.Reader, fileSize int64, filename string) (objectName string, objectURL string, err error)
-	ReplaceAvatar(ctx context.Context, userID uuid.UUID, oldObjectName string, file io.Reader, fileSize int64, filename string) (objectName string, objectURL string, err error)
-	DeleteAvatar(ctx context.Context, objectName string) error
-
-	// Audio Operations
-	UploadQuestionAudio(ctx context.Context, file io.Reader, fileSize int64, filename string) (objectName string, objectURL string, err error)
-	ReplaceQuestionAudio(ctx context.Context, oldObjectName string, file io.Reader, fileSize int64, filename string) (objectName string, objectURL string, err error)
-	DeleteQuestionAudio(ctx context.Context, objectName string) error
-
-	// Question Image Operations
-	UploadQuestionImage(ctx context.Context, questionID uuid.UUID, file io.Reader, fileSize int64, filename string) (objectName string, objectURL string, err error)
-	ReplaceQuestionImage(ctx context.Context, questionID uuid.UUID, oldObjectName string, file io.Reader, fileSize int64, filename string) (objectName string, objectURL string, err error)
-	DeleteQuestionImage(ctx context.Context, objectName string) error
 }
