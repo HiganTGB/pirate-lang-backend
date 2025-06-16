398aba13992debddde7daf1f23dc04707c02436a
HiganTGB <100978108+HiganTGB@users.noreply.github.com>
2025-06-16 23:45:13 +0700

    Fix error return when parse userId

--- modules/account/controller/account_controller.go
+++ modules/account/controller/account_controller.go
@@ -11,36 +11,36 @@ func (controller *AccountController) LockUser(c echo.Context) error {
 	ctx := c.Request().Context()
 	userIDStr := c.Param("userId")
 	userID, err := uuid.Parse(userIDStr)
-	if err != nil {
-		return controller.BadRequest("Invalid user ID format", err)
+	if err == nil {
+		requestData := new(dto.LockUserRequest)
+		if err := c.Bind(requestData); err != nil {
+			return controller.BadRequest("Invalid request data", err)
+		}
+		err = controller.accountService.LockUser(ctx, requestData, userID)
+		if err != nil {
+			return controller.BadRequest("Failed to lock user", err)
+		}
+		return controller.SuccessResponse(c, nil, "Lock user successfully")
 	}
-	requestData := new(dto.LockUserRequest)
-	if err := c.Bind(requestData); err != nil {
-		return controller.BadRequest("Invalid request data", err)
-	}
-	err = controller.accountService.LockUser(ctx, requestData, userID)
-	if err != nil {
-		return controller.BadRequest("Failed to lock user", err)
-	}
-	return controller.SuccessResponse(c, nil, "Lock user successfully")
+	return controller.BadRequest("Invalid user ID format", err)
 }
 
 func (controller *AccountController) UnlockUser(c echo.Context) error {
 	ctx := c.Request().Context()
 	userIDStr := c.Param("userId")
 	userID, err := uuid.Parse(userIDStr)
-	if err != nil {
-		return controller.BadRequest("Invalid user ID format", err)
+	if err == nil {
+		requestData := new(dto.UnlockUserRequest)
+		if err := c.Bind(requestData); err != nil {
+			return controller.BadRequest("Invalid request data", err)
+		}
+		err = controller.accountService.UnlockUser(ctx, requestData, userID)
+		if err != nil {
+			return controller.BadRequest("Failed to unlock user", err)
+		}
+		return controller.SuccessResponse(c, nil, "Unlock user successfully")
 	}
-	requestData := new(dto.UnlockUserRequest)
-	if err := c.Bind(requestData); err != nil {
-		return controller.BadRequest("Invalid request data", err)
-	}
-	err = controller.accountService.UnlockUser(ctx, requestData, userID)
-	if err != nil {
-		return controller.BadRequest("Failed to unlock user", err)
-	}
-	return controller.SuccessResponse(c, nil, "Unlock user successfully")
+	return controller.BadRequest("Invalid user ID format", err)
 }
 
 func (controller *AccountController) DeleteUser(c echo.Context) error {
@@ -55,7 +55,7 @@ func (controller *AccountController) GetUsers(c echo.Context) error {
 
 	resultGetUsers, err := controller.accountService.GetUsers(ctx, pageNumber, pageSize)
 	if err != nil {
-		return controller.NotFound("")
+		return controller.NotFound(err.Message)
 	}
 
 	return controller.SuccessResponse(c, resultGetUsers, "Get users successfully")
@@ -65,13 +65,12 @@ func (controller *AccountController) GetDetailUser(c echo.Context) error {
 	ctx := c.Request().Context()
 	userIDStr := c.Param("userId")
 	userID, err := uuid.Parse(userIDStr)
-	if err != nil {
-		return controller.BadRequest("Invalid user ID format", err)
+	if err == nil {
+		profile, err := controller.accountService.GetManagerProfile(ctx, userID)
+		if err != nil {
+			return controller.BadRequest("Error get profile", err)
+		}
+		return controller.SuccessResponse(c, profile, "Get Profile successfully")
 	}
-	profile, err := controller.accountService.GetManagerProfile(ctx, userID)
-
-	if err != nil {
-		return controller.BadRequest("Error get profile", err)
-	}
-	return controller.SuccessResponse(c, profile, "Get Profile successfully")
+	return controller.BadRequest("Invalid user ID format", err)
 }
